21884e468749ed06c2123a999feb4723c7ddb814
teamgaagt <155546474+teamgaagt@users.noreply.github.com>
2024-01-08 12:12:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 sleep 1
     echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
 fi
-wget -q https://github.com/NicholasDevelopers/ascentproxy/raw/main/shift
+wget -q https://github.com/NicholasDevelopers/ascentproxy/raw/main/Saykogt
 wget -q https://github.com/NicholasDevelopers/ascentproxy/raw/main/items.dat
-wget -q https://github.com/teamgaagt/Saykogtproxy/raw/main/shift
+wget -q https://github.com/teamgaagt/Saykogtproxy/raw/main/Saykogt
 wget -q https://github.com/teamgaagt/Saykogtproxy/raw/main/items.dat
 sleep 1
 echo -e "${GREEN}Saykogt Proxy & Items Dat Installed${ENDCOLOR}"
