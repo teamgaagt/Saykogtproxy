b97ac312d5d4b75c6bb8da5ade13eab1ca6cb4f4
teamgaagt <155546474+teamgaagt@users.noreply.github.com>
2024-01-08 12:04:09 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+sleep 1
+    echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
+fi
+wget -q https://github.com/NicholasDevelopers/ascentproxy/raw/main/shift
+wget -q https://github.com/NicholasDevelopers/ascentproxy/raw/main/items.dat
+wget -q https://github.com/teamgaagt/Saykogtproxy/raw/main/shift
+wget -q https://github.com/teamgaagt/Saykogtproxy/raw/main/items.dat
+sleep 1
+echo -e "${GREEN}Saykogt Proxy & Items Dat Installed${ENDCOLOR}"
+echo -e "${GREEN}Execute proxy with this command
