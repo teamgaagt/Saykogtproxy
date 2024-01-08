c50f14ddcd5fe3416ac886246f10ff54810ad08b
teamgaagt <155546474+teamgaagt@users.noreply.github.com>
2024-01-08 12:45:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
 fi
-wget -q https://github.com/teamgaagt/saykogtproxy/raw/main/sayko
-wget -q https://github.com/teamgaagt/saykogtproxy/raw/main/items.dat
+wget -q https://github.com/teamgaagt/Saykogtproxy/raw/main/sayko
+wget -q https://github.com/teamgaagt/Saykogtproxy/raw/main/items.dat
 sleep 1
 echo -e "${GREEN}Proxy Installed${ENDCOLOR}"
 echo -e "${GREEN}Sayko Proxy & Items Dat Installed${ENDCOLOR}"
